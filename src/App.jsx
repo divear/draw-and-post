dbc1368274dbd69d5a1667af295e78f994212dc4
lukascobit <lukyn.odehnal333@gmail.com>
2022-02-20 16:31:15 +0100

    boilerplate

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom"
+import Home from './components/Home';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
     <div className="App">
-      <h1>Picture draw and upload</h1>
+      <div onClick={() => window.location = "/"} className='header'>
+        <h1 className='headerText'>Skvost-post</h1>
+      </div>
+      <div className='content'>
+        <Router>
+          <Routes>
+            <Route exact path="/" element={<Home />}></Route>
+            <Route path="*" element={<NotFound />}></Route>
+          </Routes>
+        </Router>
+      </div>
     </div>
   );
 }
---
+++ src/components/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NotFound() {
+    return (
+        <div>
+            <h1>404 stránka nebyla nalezena</h1>
+        </div>
+    )
+}
+
+export default NotFound
