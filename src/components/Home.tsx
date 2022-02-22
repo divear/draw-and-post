f1986b061c293864290bc07cfa26a94ab5c61435
lukascobit <lukyn.odehnal333@gmail.com>
2022-02-22 20:28:16 +0100

    components

---
+++ src/components/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h1>home</h1>
+      hdsfl I love typescript
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/components/NewDrawing.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function NewDrawing() {
+  return <div>NewDrawing</div>;
+}
+
+export default NewDrawing;
