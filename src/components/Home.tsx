12baa6a76a1d64caf0538edd58a3089124e0cda2
lukascobit <lukyn.odehnal333@gmail.com>
2022-02-24 19:36:35 +0100

    basic data fetch and display

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function Home() {
+  const serverDomain = process.env.REACT_APP_SERVERDOMAIN;
+  const [data, setData] = useState<any[]>([]);
+  const [backup, setBackup] = useState([]);
+
+  useEffect(() => {
+    async function getBlogs() {
+      try {
+        const response = await fetch(serverDomain + "/drawings");
+        const jsonData = await response.json();
+        setData(jsonData);
+        setBackup(jsonData.reverse());
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getBlogs();
+  }, []);
+
   return (
     <div>
       <h1>home</h1>
-      hdsfl I love typescript
+
+      {data[0] ? (
+        data.map((d) => {
+          console.log(d);
+
+          return (
+            <div key={d.id} className="message">
+              <h5 className="date">{d.posted_date}</h5>
+              <h5>
+                <i>{d.username || "Anonym"}</i>
+              </h5>
+              <h1>{d.nazev}</h1>
+              <img className="img" src={d.img} alt="" />
+            </div>
+          );
+        })
+      ) : (
+        <h1>Načítání...</h1>
+      )}
     </div>
   );
 }
--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function NotFound() {
   return (
     <div>
-      <h1>dsj</h1>
+      <h1>Stránka nenalezena</h1>
     </div>
   );
 }
