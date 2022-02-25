32c53204d414de4c3482c58fa50e34a88949570d
lukascobit <lukyn.odehnal333@gmail.com>
2022-02-25 20:04:30 +0100

    basic drawing

--- src/components/NewDrawing.tsx
+++ src/components/NewDrawing.tsx
@@ -1,25 +1,28 @@
 import React from "react";
+import Drawing from "../Drawing";
 
 function NewDrawing() {
-  return (
-    <div>
-      <h1>Novej obrázek</h1>
+    return (
+        <div>
+            <h1>Novej obrázek</h1>
 
-      <form className="addForm" action="">
-        <div className="usernameDiv">
-          <label htmlFor="username">Přezdívka</label>
-          <br />
-          <input type="text" id="username" />
-        </div>
+            <form className="addForm" action="">
+                <div className="usernameDiv">
+                    <label htmlFor="username">Přezdívka</label>
+                    <br />
+                    <input type="text" id="username" />
+                </div>
 
-        <div className="bodyDiv">
-          <label htmlFor="nazev">Název obrázku</label>
-          <br />
-          <input type="text" id="nazev" />
+                <div className="bodyDiv">
+                    <label htmlFor="nazev">Název obrázku</label>
+                    <br />
+                    <input type="text" id="nazev" />
+                </div>
+                <Drawing />
+                <button>Poslat</button>
+            </form>
         </div>
-      </form>
-    </div>
-  );
+    );
 }
 
 export default NewDrawing;
