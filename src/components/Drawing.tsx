c9cb892c47044a9935e09cf6a1a7cd236481df9d
lukascobit <lukyn.odehnal333@gmail.com>
2022-12-22 11:17:46 +0100

    GET works

--- src/components/Drawing.tsx
+++ src/components/Drawing.tsx
@@ -2,80 +2,80 @@ import React, { useRef, useState } from "react";
 let gcanvas;
 
 function Drawing() {
-    let Xpos: number;
-    let Ypos: number;
-    let isDown = false;
-    const [lineWidth, setLineWidth] = useState(50);
-    const [color, setColor] = useState("black");
+	let Xpos: number;
+	let Ypos: number;
+	let isDown = false;
+	const [lineWidth, setLineWidth] = useState(50);
+	const [color, setColor] = useState("black");
 
-    const HEIGHT =
-        window.innerWidth > 820 ? window.innerHeight / 1.2 : window.innerWidth;
-    const WIDTH =
-        window.innerWidth > 820 ? window.innerWidth / 2 : window.innerWidth;
+	const HEIGHT =
+		window.innerWidth > 820 ? window.innerHeight / 1.2 : window.innerWidth;
+	const WIDTH =
+		window.innerWidth > 820 ? window.innerWidth / 2 : window.innerWidth;
 
-    onmousedown = function (e) {
-        isDown = true;
-        this.onmousemove = (e) => {
-            if (isDown) {
-                Xpos = e.offsetX;
-                Ypos = e.offsetY;
-            }
+	onmousedown = function (e) {
+		isDown = true;
+		this.onmousemove = (e) => {
+			if (isDown) {
+				Xpos = e.offsetX;
+				Ypos = e.offsetY;
+			}
 
-            this.onmouseup = () => {
-                isDown = false;
-            };
-        };
-    };
-    if (window.innerWidth < 820) {
-        window.ontouchmove = function (e) {
-            console.log(WIDTH);
+			this.onmouseup = () => {
+				isDown = false;
+			};
+		};
+	};
+	if (window.innerWidth < 820) {
+		window.ontouchmove = function (e) {
+			console.log(WIDTH);
 
-            Xpos = e.touches[0].clientX;
-            Ypos = e.touches[0].clientY - HEIGHT;
-        };
-    }
+			Xpos = e.touches[0].clientX;
+			Ypos = e.touches[0].clientY - HEIGHT;
+		};
+	}
 
-    const canvas = useRef(null);
+	const canvas = useRef(null);
 
-    function Draw() {
-        const c = canvas.current && canvas.current.getContext("2d");
-        gcanvas = canvas.current;
+	function Draw() {
+		const c = canvas.current && canvas.current.getContext("2d");
+		gcanvas = canvas.current;
 
-        if (c) {
-            c.fillStyle = color;
-            c.beginPath();
-            c.arc(Xpos, Ypos, lineWidth, 0, 2 * Math.PI);
-            c.fill();
-        }
-    }
-    return (
-        <div className="drawing">
-            <input
-                onChange={(e) => setLineWidth(+e.target.value)}
-                value={lineWidth}
-                type="range"
-                className="floatRight"
-                min={1}
-                max={100}
-                title="Tloušťka tahu"
-            />
-            <input
-                type="color"
-                value={color}
-                onChange={(e) => setColor(e.target.value)}
-                title="Barva tahu"
-                className="colorInput"
-            />
-            <canvas
-                id="canvas"
-                onMouseMove={Draw}
-                onTouchMove={Draw}
-                ref={canvas}
-                width={WIDTH}
-                height={HEIGHT}
-            />
-        </div>
-    );
+		if (c) {
+			c.fillStyle = color;
+			c.beginPath();
+			c.arc(Xpos, Ypos, lineWidth, 0, 2 * Math.PI);
+			c.fill();
+		}
+	}
+	return (
+		<div className="drawing">
+			<input
+				onChange={(e) => setLineWidth(+e.target.value)}
+				value={lineWidth}
+				type="range"
+				className="floatRight"
+				min={1}
+				max={100}
+				title="Tloušťka tahu"
+			/>
+			<input
+				type="color"
+				value={color}
+				onChange={(e) => setColor(e.target.value)}
+				title="Barva tahu"
+				className="colorInput"
+			/>
+			<canvas
+				id="canvas"
+				onMouseMove={Draw}
+				onTouchMove={Draw}
+				ref={canvas}
+				width={WIDTH}
+				height={HEIGHT}
+			/>
+		</div>
+	);
 }
 
 export default Drawing;
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,116 +3,117 @@ import Modal from "./Modal";
 import defaultPfp from "./imgs/defaultPfp.png";
 
 function Home() {
-    const isPhone = window.innerWidth < 840;
-    const pfp = localStorage.getItem("pfp") || defaultPfp;
-    const modalRef = useRef();
+	const isPhone = window.innerWidth < 840;
+	const pfp = localStorage.getItem("pfp") || defaultPfp;
+	const modalRef = useRef();
 
-    const serverDomain = process.env.REACT_APP_SERVERDOMAIN;
-    const [data, setData] = useState<any[]>([]);
-    const [modalVis, setModalVis] = useState(false);
-    const [modalPic, setModalPic] = useState("");
+	const serverDomain = process.env.REACT_APP_SERVERDOMAIN;
+	const [data, setData] = useState<any[]>([]);
+	const [modalVis, setModalVis] = useState(false);
+	const [modalPic, setModalPic] = useState("");
 
-    useEffect(() => {
-        async function getBlogs() {
-            try {
-                const response = await fetch(serverDomain + "/drawings");
-                const jsonData = await response.json();
-                setData(jsonData.reverse());
-                console.log(jsonData);
-            } catch (error) {
-                console.log(error);
-            }
-        }
-        getBlogs();
-    }, []);
+	useEffect(() => {
+		async function getBlogs() {
+			try {
+				const response = await fetch(serverDomain + "/drawings");
 
-    onkeydown = (e) => {
-        if (e.key === "Escape") {
-            setModalVis(false);
-        }
-    };
+				const jsonData = await response.json();
+				setData(jsonData.reverse());
+				console.log(jsonData);
+			} catch (error) {
+				console.log(error);
+			}
+		}
+		getBlogs();
+	}, []);
 
-    return (
-        <div>
-            <title>Malo</title>
+	onkeydown = (e) => {
+		if (e.key === "Escape") {
+			setModalVis(false);
+		}
+	};
 
-            <div ref={modalRef} className={modalVis ? "" : "no"}>
-                <Modal />
-            </div>
+	return (
+		<div>
+			<title>Malo</title>
 
-            <div className="profileMin">
-                <h1
-                    onClick={() => setModalVis(true)}
-                    className={
-                        localStorage.getItem("username") ||
-                        localStorage.getItem("pfp")
-                            ? ""
-                            : "no"
-                    }
-                >
-                    {localStorage.getItem("username")}
-                    <img className="pfp" src={pfp} alt="Icon" />
-                </h1>
-                <button
-                    className={
-                        localStorage.getItem("username") ||
-                        localStorage.getItem("pfp")
-                            ? "no"
-                            : ""
-                    }
-                    onClick={() => setModalVis(true)}
-                >
-                    Přihlásit se
-                </button>
-            </div>
-            <button
-                onClick={() => (window.location.href = "/novyObrazek")}
-                className="addDrawing"
-            >
-                Namalovat nový
-            </button>
-            <br />
-            <div
-                onClick={() => setModalPic(null)}
-                className={modalPic && !isPhone ? "modalPicParent" : "no"}
-            >
-                <img className="modalPic" src={modalPic} alt="" />
-            </div>
-            <div className="messages">
-                {data[0] ? (
-                    data.map((d) => {
-                        return (
-                            <div
-                                onClick={() => setModalPic(d.img)}
-                                key={d.id}
-                                className="message"
-                            >
-                                <h5>
-                                    <img
-                                        className="smallPfp"
-                                        src={d.pfp}
-                                        alt="👤"
-                                    />
-                                    <i>{d.username || "Anonym"}</i>
-                                </h5>
-                                <h1>{d.nazev}</h1>
-                                <img className="img" src={d.img} alt="" />
-                                <h5 className="date">{d.posted_date}</h5>
-                            </div>
-                        );
-                    })
-                ) : (
-                    <h1>Načítání...</h1>
-                )}
-            </div>
-            <div className="footer">
-                Lukáš Odehnal 2022
-                <a className="floatRight" href="/feedback">
-                    Feedback
-                </a>
-            </div>
-        </div>
-    );
+			<div ref={modalRef} className={modalVis ? "" : "no"}>
+				<Modal />
+			</div>
+
+			<div className="profileMin">
+				<h1
+					onClick={() => setModalVis(true)}
+					className={
+						localStorage.getItem("username") ||
+						localStorage.getItem("pfp")
+							? ""
+							: "no"
+					}
+				>
+					{localStorage.getItem("username")}
+					<img className="pfp" src={pfp} alt="Icon" />
+				</h1>
+				<button
+					className={
+						localStorage.getItem("username") ||
+						localStorage.getItem("pfp")
+							? "no"
+							: ""
+					}
+					onClick={() => setModalVis(true)}
+				>
+					Přihlásit se
+				</button>
+			</div>
+			<button
+				onClick={() => (window.location.href = "/novyObrazek")}
+				className="addDrawing"
+			>
+				Namalovat nový
+			</button>
+			<br />
+			<div
+				onClick={() => setModalPic(null)}
+				className={modalPic && !isPhone ? "modalPicParent" : "no"}
+			>
+				<img className="modalPic" src={modalPic} alt="" />
+			</div>
+			<div className="messages">
+				{data[0] ? (
+					data.map((d) => {
+						return (
+							<div
+								onClick={() => setModalPic(d.img)}
+								key={d.id}
+								className="message"
+							>
+								<h5>
+									<img
+										className="smallPfp"
+										src={d.pfp}
+										alt="👤"
+									/>
+									<i>{d.username || "Anonym"}</i>
+								</h5>
+								<h1>{d.nazev}</h1>
+								<img className="img" src={d.img} alt="" />
+								<h5 className="date">{d.posted_date}</h5>
+							</div>
+						);
+					})
+				) : (
+					<h1>Načítání...</h1>
+				)}
+			</div>
+			<div className="footer">
+				Lukáš Odehnal 2022
+				<a className="floatRight" href="/feedback">
+					Feedback
+				</a>
+			</div>
+		</div>
+	);
 }
 
 export default Home;
